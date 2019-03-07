07fb0e7a856cc246b09d509429a7b412e0202f23
Andrew Truschel <atruschel@salsify.com>
2019-03-06 21:03:29 -0500

    update to Reviews feature tests

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -7,160 +7,154 @@ describe "creating reviews -", type: :feature do
   it "authenticated user creates new review with all required fields populated" do
     user
     visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test1@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
+    find_link('sign in').click
+    fill_in('email', with: 'test1@gmail.com')
+    fill_in('password', with: 'blahblah1234')
+    find_button('log in').click
     visit '/reviews/new'
     fill_in 'review_title', with: 'Test title'
     fill_in 'review_body', with: 'here is a sample description'
-    click_button 'submit'
-    save_and_open_page
-    expect(page).to have_content 'Test title'
-    expect(page).to have_content 'here is a sample description'
-    expect(page).to have_button 'edit review'
-    expect(page).to have_button 'delete review'
+    find_button('submit').click
+    page.has_content?('Test title')
+    page.has_content?('here is a sample description')
 
   end
 
-  it "authenticated user creates new review without all required fields populated" do
-    user
-    visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test1@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
-    visit '/reviews/new'
-    fill_in 'review_body', with: 'here is a sample description'
-    click_button 'submit'
+   it "authenticated user creates new review without all required fields populated" do
+     user
+     visit root_path
+     find_link('sign in').click
+     fill_in('email', with: 'test1@gmail.com')
+     fill_in('password', with: 'blahblah1234')
+     find_button('log in').click
+     visit '/reviews/new'
+     fill_in('review_body', with: 'here is a sample description')
+     find_button('submit').click
 
-    expect(page).to have_content "Title can't be blank"
-  end
+     expect(page).to have_content "Title can't be blank"
+   end
 
-  it "unauthenticated user cannot create new review" do
-    visit 'root_path'
-    click_link ' add review'
-    expect(page).to have_content 'You do not have access to this page'
-  end
-
+   it "unauthenticated user cannot create new review" do
+     visit 'root_path'
+     find_link(' add review').click
+     expect(page).to have_content 'You do not have access to this page'
+   end
 end
 
-describe "editing / deleting reviews -" do
-  before :each do
-    user
-    click_link 'sign in'
-    fill_in 'email', with: 'test1@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
-    visit '/reviews/new'
-    fill_in 'review_title', with: 'Test title'
-    fill_in 'review_body', with: 'here is a sample description'
-    click_button 'submit'
-    click_link 'sign out'
-  end
+ describe "editing / deleting reviews -" do
+   before :each do
+     user
+     find_link('sign in').click
+     fill_in 'email', with: 'test1@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_button('log in').click
+     visit '/reviews/new'
+     fill_in 'review_title', with: 'Test title'
+     fill_in 'review_body', with: 'here is a sample description'
+     find_button('submit').click
+     find_link('sign out').click
+   end
 
-  it "allows authenticated author to edit their review successfully" do
-    user
+   it "allows authenticated author to edit their review successfully" do
+     user
 
-    #login
-    visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test1@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
+     #login
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test1@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_button('log in').click
 
-    #edit review
-    click_link 'all reviews'
-    click_link 'Test title'
-    click_button 'edit review'
-    fill_in 'review_title', with: 'Second test title'
-    click_button 'submit'
-    expect(page).to have_content 'Second test title'
-  end
+     #edit review
+     find_link('all reviews').click
+     find_link('Test title').click
+     find_button('edit review').click
+     fill_in 'review_title', with: 'Second test title'
+     find_button('submit').click
+     expect(page).to have_content 'Second test title'
+   end
 
-  it "user who is not the author of review can't edit review successfully" do
-    user
-    user_two = FactoryBot.create(:user)
-    visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test2@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
+   it "user who is not the author of review can't edit review successfully" do
+     user
+     user_two = FactoryBot.create(:user)
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test2@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_button('log in').click
 
-    click_link 'all reviews'
-    click_link 'Test title'
-    expect(page).not_to have_button 'edit review'
-  end
+     find_link('all reviews').click
+     find_link('Test title').click
+     expect(page).not_to have_button 'edit review'
+   end
 
-  it "allows authenticated author to delete their review successfully" do
-    user
-    visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test1@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
+   it "allows authenticated author to delete their review successfully" do
+     user
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test1@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_button('log in').click
 
-    #edit review
-    click_link 'all reviews'
-    click_link 'Test title'
-    click_button 'delete review'
-    click_button 'OK'
-    expect(page).to have_content 'Your question has been deleted'
-  end
+     #edit review
+     find_link('all reviews').click
+     find_link('Test title').click
+     find_button('delete review').click
+     find_button('OK').click
+     expect(page).to have_content 'Your question has been deleted'
+   end
 
-  it "user who is not the author of review cannot delete review" do
-    user
-    user_two = FactoryBot.create(:user)
-    visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test2@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
+   it "user who is not the author of review cannot delete review" do
+     user
+     user_two = FactoryBot.create(:user)
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test2@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_button('log in').click
 
-    click_link 'all reviews'
-    click_link 'Test title'
-    expect(page).not_to have_button 'delete review'
-  end
+     find_link('all reviews').click
+     find_link('Test title').click
+     expect(page).not_to have_button 'delete review'
+   end
+ end
 
-end
+ describe "voting for reviews" do
+   it "allows authenticated user to vote up or down a review" do
+     user
 
-describe "voting for reviews" do
-  it "allows authenticated user to vote up or down a review" do
-    user
+     #login
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test1@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_link('log in').click
 
-    #login
-    visit root_path
-    click_link 'sign in'
-    fill_in 'email', with: 'test1@gmail.com'
-    fill_in 'password', with: 'blahblah1234'
-    click_button 'log in'
+     #up_vote review
+     find_link('all reviews').click
+     find_link('Test title').click
+     find_link('thumbs_up').click
+     expect(page).to have_content 'Thanks for voting'
 
-    #up_vote review
-    click_link 'all reviews'
-    click_link 'Test title'
-    click_link 'thumbs_up'
-    expect(page).to have_content 'Thanks for voting'
+     #down_vote review
+     find_link('thumbs_down').click
+     expect(page).to have_content 'Thanks for voting'
+   end
 
-    #down_vote review
-    click_link 'thumbs_down'
-    expect(page).to have_content 'Thanks for voting'
-  end
+   it "unauthenticated user can't vote for review" do
+     visit root_path
+     find_link('all reviews').click
+     find_link('Test title').click
+     expect(page).not_to have_link 'thumbs_up'
+     expect(page).not_to have_link 'thumbs_down'
+   end
+ end
 
-  it "unauthenticated user can't vote for review" do
-    visit root_path
-    click_link 'all reviews'
-    click_link 'Test title'
-    expect(page).not_to have_link 'thumbs_up'
-    expect(page).not_to have_link 'thumbs_down'
-  end
-
-end
-
-describe "searching for reviews -" do
-  it "allows users to search for reviews by substring" do
-    visit root_path
-    fill_in 'q', with: 'Test'
-    expect(page).to have_content 'search results for "Test"'
-    expect(page).to have_link 'Test title'
-  end
-end
+ describe "searching for reviews -" do
+   it "allows users to search for reviews by substring" do
+     visit root_path
+     fill_in 'q', with: 'Test'
+     expect(page).to have_content 'search results for "Test"'
+     expect(page).to have_link 'Test title'
+   end
+ end
