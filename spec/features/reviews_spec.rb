0fa23393b5a7581c4549b85d1aa13cfb8e363e27
Andrew Truschel <atruschel@salsify.com>
2019-03-09 16:51:48 -0500

    add tests to users and reviews specs, update sidebar partial

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -28,16 +28,15 @@ describe "creating reviews -", type: :feature do
      fill_in('password', with: 'blahblah1234')
      find_button('log in').click
      visit '/reviews/new'
-     fill_in('review_body', with: 'here is a sample description')
+     fill_in 'review_body', with: 'here is a sample description'
      find_button('submit').click
-
-     expect(page).to have_content "Title can't be blank"
+     page.has_content?("Title can't be blank")
    end
 
    it "unauthenticated user cannot create new review" do
-     visit 'root_path'
-     find_link(' add review').click
-     expect(page).to have_content 'You do not have access to this page'
+     visit root_path
+     find_link('add_new_review').click
+     page.has_content?('You do not have access to this page')
    end
 end
 
@@ -71,7 +70,7 @@ end
      find_button('edit review').click
      fill_in 'review_title', with: 'Second test title'
      find_button('submit').click
-     expect(page).to have_content 'Second test title'
+     page.has_content?('Second test title')
    end
 
    it "user who is not the author of review can't edit review successfully" do
@@ -139,6 +138,48 @@ end
      #down_vote review
      find_link('thumbs_down').click
      expect(page).to have_content 'Thanks for voting'
+   end
+
+   it "prevents authenticated user from voting up a review more than once" do
+     user
+
+     #login
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test1@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_link('log in').click
+
+     #up_vote review
+     find_link('all reviews').click
+     find_link('Test title').click
+     find_link('thumbs_up').click
+     expect(page).to have_content 'Thanks for voting'
+
+     #up_vote again
+     find_link('thumbs_up').click
+     expect(page).to have_content "You've already voted up this review!"
+   end
+
+   it "prevents authenticated user from voting down a review more than once" do
+     user
+
+     #login
+     visit root_path
+     find_link('sign in').click
+     fill_in 'email', with: 'test1@gmail.com'
+     fill_in 'password', with: 'blahblah1234'
+     find_link('log in').click
+
+     #up_vote review
+     find_link('all reviews').click
+     find_link('Test title').click
+     find_link('thumbs_down').click
+     expect(page).to have_content 'Thanks for voting'
+
+     #up_vote again
+     find_link('thumbs_down').click
+     expect(page).to have_content "You've already voted down this review!"
    end
 
    it "unauthenticated user can't vote for review" do
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -44,4 +44,34 @@ feature "user auth:" do
     expect(page).to have_link('sign out')
   end
 
+  scenario "current user signs out" do
+    email = "test1@gmail.com"
+    pw = "password123"
+
+    #create new user and then logout
+    visit root_path
+    click_link "sign in"
+    click_link "sign up"
+    fill_in 'first name', with: "test"
+    fill_in 'last name', with: "user"
+    fill_in 'email', with: email
+    fill_in 'password', with: pw
+    fill_in 'password confirmation', with: pw
+    click_button 'sign up'
+    expect(page).to have_content "Welcome! You have signed up successfully."
+    click_link 'sign out'
+
+    #sign in as previously created user
+    click_link "sign in"
+    fill_in 'email', with: email
+    fill_in 'password', with: pw
+    click_button 'log in'
+    expect(page).to have_content "Signed in successfully."
+    expect(page).to have_link('sign out')
+
+    click_link "sign out"
+    expect(page).to have_content "Signed out successfully."
+    expect(page).to have_link('sign in')
+  end
+
 end
